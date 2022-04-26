7f4b0f150f7906af255cd919883c65195b142f76
Alexander Boström <alexander.bostrom94@gmail.com>
2022-04-26 10:36:52 +0200

    file upload till api/upload

--- api/api.py
+++ api/api.py
@@ -1,8 +1,22 @@
-import time
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'npy'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/api/upload', methods = ['POST'])
+def onFileUpload():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #return redirect(url_for('download_file', name=filename))
+    return "done"
