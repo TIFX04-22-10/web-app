01cade55ea3b5d5e1e88c12a26ee2dbedfada39d
Alexander Boström <alexander.bostrom94@gmail.com>
2022-05-03 09:44:34 +0200

    added gif split function and a delete button

--- api/api.py
+++ api/api.py
@@ -1,12 +1,27 @@
-import os
+import os, shutil
 from flask import Flask, flash, request, redirect, url_for
+from PIL import Image
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'upload'
+OUTPUT_FOLDER = 'output'
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'npy'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+
+def extractFrames(inGif, outFolder):
+    frame = Image.open(inGif)
+    nframes = 0
+    while frame:
+        frame.save( '%s/%s-%s.png' % (outFolder, 'output', nframes ) , 'PNG')
+        nframes += 1
+        try:
+            frame.seek( nframes )
+        except EOFError:
+            break;
+    return True
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -18,5 +33,19 @@ def onFileUpload():
     if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            extractFrames('bubbles.gif', 'output')
             #return redirect(url_for('download_file', name=filename))
     return "done"
+
+@app.route("/delete", methods=['POST'])
+def empty_folder():
+    for filename in os.listdir(app.config['OUTPUT_FOLDER']):
+        file_path = os.path.join(app.config['OUTPUT_FOLDER'], filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    return redirect(request.referrer)
