17c46a8ed830f571c88fc6492dc431a53b68f254
tijanab <baractijana@gmail.com>
2022-05-08 21:44:54 +0200

    GH-1: Basic interface

--- src/features/Header.js
+++ src/features/Header.js
@@ -1,7 +1,25 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from '../resources/Logo.png';
+import './Header.css';
+
 export const Header = () => {
-    return (
-        <h1>RunningPose</h1>
-    )
-}
+	return (
+		<>
+			<Navbar expand="lg" className="navbar">
+				<Container>
+					<Navbar.Brand href="#home">
+            <img src={logo} width="200px" alt="RunningPose"/>
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="nav">
+							<Nav.Link href="">About</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
 
 export default Header;
--- src/features/Main.js
+++ src/features/Main.js
@@ -1,10 +1,14 @@
 import VideoUpload from "./VideoUpload";
+import logo from '../resources/BigLogo.png';
+import './Main.css';
+
 
 export const Main = () => {
     return (
-        <>
+        <div className="container">
+            <img src={logo} alt='RunningPose' className="logo"/>
             <VideoUpload/>
-        </>
+        </div>
     )
 }
 
