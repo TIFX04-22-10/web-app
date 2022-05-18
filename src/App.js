1c9c93104e1c721bb6d768fca84c5d096d1f7456
Alexander Boström <alexander.bostrom94@gmail.com>
2022-05-18 14:43:29 +0200

    add scrolling animation and output example

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './features/Main';
 import Header from './features/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
     return (
-        <div className="App"> 
+        <> 
             {/* <Header /> */}
             <Main />
-        </div>
+        </>
     );
 }
 
