b6232f5142b901341fb7a8a9a1aa25e09650b70a
Alexander Boström <alexander.bostrom94@gmail.com>
2022-05-18 15:03:06 +0200

    re-add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from './features/Main';
 import Header from './features/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
