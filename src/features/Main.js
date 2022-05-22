5292daa734240f052eef5ab41b39bdb625379f02
Tijana Barac <baractijana@gmail.com>
2022-05-22 23:31:49 +0200

    GH-11: Display angles with mock data

--- src/features/Main.js
+++ src/features/Main.js
@@ -1,23 +1,16 @@
 import VideoUpload from "./VideoUpload";
 import './Main.css';
 import Visualization from "./Visualization";
-import Grid from '@mui/material/Grid';
+import Angles from "./Angles";
 
 
 export const Main = () => {
     return (
         <>
-{/*             <Grid container spacing={2}>
-                <Grid item xs={6} md={4}>
-                    <div><VideoUpload /></div>
-                </Grid>
-                <Grid item xs={6} md={8}>
-                    <div><Visualization /></div>
-                </Grid>
-            </Grid> */}
             <div className="container">
                 <VideoUpload/>
                 <Visualization className="result"/>
+                <Angles/>
             </div>
         </>
     )
